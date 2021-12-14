4f25ca59fa82d4953f2b6afb45b13e49a25f52f5
unknown <C:\Users\ i\AppData\Roaming\The Bat!\Mail>
2021-12-14 19:39:10 +0200

    init

---
+++ VigenèreDecoder/VigenèreDecoder/Decryption.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VigenèreDecoder
+{
+    class Decryption
+    {
+       
+        public static string Decode(string _inputValue, string _keyGen)
+        {
+            char[] charValue = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                                             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                                             't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '1',
+                                             '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+
+            int _arrayElement = charValue.Length;
+
+            _inputValue = _inputValue.ToLower();
+            _keyGen = _keyGen.ToLower();
+
+            string _result = "";
+
+            int _keyGen_index = 0;
+
+            foreach (char symbol in _inputValue)
+            {
+                int _sourceMessageCharacter = (Array.IndexOf(charValue, symbol) + _arrayElement - Array.IndexOf(charValue, _keyGen[_keyGen_index])) % _arrayElement;
+
+                _result += charValue[_sourceMessageCharacter];
+
+                _keyGen_index++;
+
+                if ((_keyGen_index + 1) == _keyGen.Length)
+                {
+                    _keyGen_index = 0;
+                }                   
+            }
+            return _result;
+        }
+    }
+}
---
+++ VigenèreDecoder/VigenèreDecoder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace VigenèreDecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _enteredWord;
+            Console.WriteLine("Enter the word you want to encrypt ");
+            _enteredWord = Console.ReadLine();
+            
+            string _KeyGen = KeyGen.GenerateKeyWord(7, 2);
+            Console.WriteLine("Your key for encryption " + _KeyGen);
+
+            string _EncryptedWord = Encrypt.Encode(_enteredWord, _KeyGen);
+
+            Console.WriteLine("Your encrypted word " + _EncryptedWord);
+            Console.WriteLine("Would you like to decipher your word? yes/no");
+            string _answerOption = Console.ReadLine();
+
+            switch (_answerOption)
+            {
+                case "yes":
+                    Console.WriteLine(Decryption.Decode(_EncryptedWord, _KeyGen));
+                    break;
+
+                case "no":
+                    Console.WriteLine();
+                    break;
+
+                default:
+                    Console.WriteLine("You entered an action that was not intended! ");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}

9091788d3dae49208002bff051cc0a01f1470677
NataliMi8 <92589644+NataliMi8@users.noreply.github.com>
2022-02-19 11:24:04 +0200

    Add files via upload

---
+++ XUnitTestProject_15.02.2022/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject_15._02._2022
+{
+    public class Class1
+    {       
+        public static double ForTest(double a, double b, double c)
+        {
+            double x = b * b - 4 * a * c;
+
+            return x;
+        }      
+    }
+}
